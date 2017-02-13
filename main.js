d89c540e86bc17d1a25b1b460d0dac4b9293b43b
mofanjunoo <mofanjunoo@gmail.com>
2017-02-13 09:41:14 +0800

    初次创建工程
    工程构件

---
+++ main.js
@@ -0,0 +1,18 @@
+//express
+var express = require('express');
+var app = express();
+
+//view engine
+var exphbs = require('express-handlebars');
+app.engine('handlebars',exphbs({defaultLayout:'layout'}));
+app.set('view engine','handlebars');
+
+//res dir
+app.use(express.static(__dirname + '/public'));
+
+//For Project File
+require('./routes.js')(app);
+
+app.listen(8080,function(){
+	console.log('✨ app start at port 8080 ✨');
+})
